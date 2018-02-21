42c586108a67089a210c213b82f43f5a16b09031
Solene <demarsolene@gmail.com>
2018-02-21 16:36:09 +0100

    tests status code

---
+++ src/test/java/BouteilleGetIT.java
@@ -0,0 +1,43 @@
+package fr.univ_smb.isc.m2.integration_tests;
+
+import java.io.BufferedReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static org.apache.http.HttpStatus.SC_NOT_FOUND;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BouteilleGetIT {
+
+
+    @Test
+    public void should_200_On_Existing_Bouteille() throws IOException, URISyntaxException {
+
+        HttpUriRequest request = new HttpGet(new URL("http://localhost:"+8080+"/api/bouteilles/" + 6).toURI());
+
+        HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+        assertThat(response.getStatusLine().getStatusCode()).isEqualTo(SC_OK);
+        
+    }
+
+    @Test
+    public void should_404_On_Non_Existing_Bouteille() throws IOException, URISyntaxException {
+
+        HttpUriRequest request = new HttpGet(new URL("http://localhost:"+8080+"/api/bouteilles/" + 99999).toURI());
+
+        HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+        assertThat(response.getStatusLine().getStatusCode()).isEqualTo(SC_NOT_FOUND);
+
+    }
+}
